bef0003525f881bf0fae673b5a58aacdead6ccd9
Denis <denis.v.simon@gmail.com>
2024-11-14 08:44:55 +0300

    Update PredictionBuilderTest.php
    PHPUnit 11.4.3

--- tests/PredictionBuilderTest.php
+++ tests/PredictionBuilderTest.php
@@ -2,6 +2,7 @@
 
 require_once '../src/PredictionBuilder.php';
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use PredictionBuilder\PredictionBuilder;
 
@@ -9,9 +10,15 @@ class PredictionBuilderTest extends TestCase
 {
     // 1 ------------------------------------------------------------------------>
     
-    /**
-     * @dataProvider getResultData
-     */
+    public static function resultData() {
+        $data = [[1,20],[2,70],[2,45],[3,81],[5,73],[6,80],[7,110]];        
+        return [
+            [4.5, $data, 76.65],
+            [8, $data, 113.27274]
+        ];
+    }
+
+    #[DataProvider('resultData')]
     public function testGetResult($x, $data, $expected_result) {
         $prediction = new PredictionBuilder($x, $data);
         $result = $prediction->build();
@@ -19,31 +26,21 @@ class PredictionBuilderTest extends TestCase
         $this->assertEquals($result->y, $expected_result);
     }
     
-    public function getResultData() {
-    	$data = [[1,20],[2,70],[2,45],[3,81],[5,73],[6,80],[7,110]];    	
-        return [
-            [4.5, $data, 76.65],
-            [8, $data, 113.27274]
-        ];
-    }
-    
     // 2 ------------------------------------------------------------------------>
 	
-    /**
-     * @dataProvider exceptionData
-     */
-    public function testException($x, $data) {
-        $this->expectException(InvalidArgumentException::class);
-        
-        $prediction = new PredictionBuilder($x, $data);
-        $result = $prediction->build();
-    }
-        
-    public function exceptionData() {
+    public static function exceptionData() {
         return [
             [4.5, [[1,20],[2,70]]],
             [4.5, [[1,20],[2,70],[2],[3,81],[5,73],[6,80],[7,110]]],
             [4.5, [[1,20],[2,70],[],[3,81],[5,73],[6,80],[7,110]]]
         ];
+    }
+
+    #[DataProvider('exceptionData')]
+    public function testException($x, $data) {
+        $this->expectException(InvalidArgumentException::class);
+        
+        $prediction = new PredictionBuilder($x, $data);
+        $result = $prediction->build();
     }
 }
